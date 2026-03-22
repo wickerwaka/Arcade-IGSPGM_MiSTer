c7eab3d40453e9c61c2ce294f6d24a093715679c
Martin Donlon <github-martin@donlons.com>
2026-03-22 11:59:54 -0700

    Fixed DE-10 compilation
    Switch system clock to 50Mhz, SDRAM to 100Mhz

--- rtl/pll/pll_0002.v
+++ rtl/pll/pll_0002.v
@@ -18,14 +18,14 @@ module  pll_0002(
 );
 
 	altera_pll #(
-		.fractional_vco_multiplier("true"),
+		.fractional_vco_multiplier("false"),
 		.reference_clock_frequency("50.0 MHz"),
 		.operation_mode("direct"),
 		.number_of_clocks(2),
-		.output_clock_frequency0("106.744000 MHz"),
+		.output_clock_frequency0("100.000000 MHz"),
 		.phase_shift0("0 ps"),
 		.duty_cycle0(50),
-		.output_clock_frequency1("53.372000 MHz"),
+		.output_clock_frequency1("50.000000 MHz"),
 		.phase_shift1("0 ps"),
 		.duty_cycle1(50),
 		.output_clock_frequency2("0 MHz"),
